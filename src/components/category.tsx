fb780fc6cb1fc22c31c55f3b59fb7f7c83450d63
Rez1stor <bosco@Boscos-MacBook-Pro.local>
2024-01-10 09:08:20 +0200

    added footer

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -1,27 +1,25 @@
-import { PiDogLight, PiCatLight, PiAlienLight } from "react-icons/pi";
+import { PiDogLight, PiCatLight, PiAlienLight } from 'react-icons/pi';
 
 export default function Category() {
-    return (
-        <div className="max-w-[91%] py-32 mx-auto">
-            <div className="text-center">
-                <h1>Category</h1>
-            </div>
-            <div className="grid grid-cols-3 gap-4 h-80 px-24 py-4">
-                <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
-                    <PiDogLight className="size-40 content-center"/>
-                    <h2 className="font-bold text-center">Dogs</h2>
-                </button>
-                <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
-                    <PiCatLight className="size-40 content-center"/>
-                    <h2 className="font-bold text-center">Cats</h2>
-                </button>
-                <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
-                    <PiAlienLight className="size-40 content-center"/>
-                    <h2 className="font-bold text-center">Others</h2>
-                </button>
-
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="z-0 max-w-[91%] py-32 mx-auto">
+      <div className="text-center">
+        <h1>Category</h1>
+      </div>
+      <div className="grid grid-cols-3 gap-4 h-80 px-24 py-4">
+        <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
+          <PiDogLight className="size-40 content-center" />
+          <h2 className="font-bold text-center">Dogs</h2>
+        </button>
+        <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
+          <PiCatLight className="size-40 content-center" />
+          <h2 className="font-bold text-center">Cats</h2>
+        </button>
+        <button className="col-span-1 bg-white drop-shadow-lg rounded-lg flex justify-center items-center align-stretch flex-col h-max py-[42px]">
+          <PiAlienLight className="size-40 content-center" />
+          <h2 className="font-bold text-center">Others</h2>
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,25 +1,31 @@
-import dog from "../assets/dogF.png";
+import dog from '../assets/dogF.png';
 
 export default function HeroSection() {
-    return (
-        <>
-            <div className="grid max-w-[91%] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-48 lg:grid-cols-12">
-                <div className="mr-auto place-self-center lg:col-span-6 pt-52"> 
-                    <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white   "> 
-                        Find your new best friend
-                    </h1>
-                    <hr className="w-[32rem] h-1 my-8 bg-rede border-0 rounded md:my-10 dark:bg-gray-700" />
-                    <h3 className="max-w-2xl mb-6 text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                    It’s never too late to fall in <b className="text-rede">love</b>
-                    </h3>
-                    <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-bold text-center text-white rounded-lg bg-rede hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                     Get started
-                    </a>
-                </div>
-                <div className="hidden lg:mt-0 lg:col-span-6 lg:flex">
-                 <img src={dog} className="absolute left-[696px] top-[80px] w-[733px] h-[901px]" />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="grid max-w-[91%] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-48 my-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-6 pt-22">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white   ">
+            Find your new best friend
+          </h1>
+          <hr className="w-[32rem] h-1 my-8 bg-rede border-0 rounded md:my-10 dark:bg-gray-700" />
+          <h3 className="max-w-2xl mb-6 text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            It’s never too late to fall in <b className="text-rede">love</b>
+          </h3>
+          <a
+            href="#"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-bold text-center text-white rounded-lg bg-rede hover:drop-shadow-lg hover:bg-primary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+          >
+            Get started
+          </a>
+        </div>
+        <div className="hidden lg:mt-0 lg:col-span-6 lg:flex">
+          <img
+            src={dog}
+            className="absolute left-[696px] top-[80px] w-[733px] h-[901px]"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
