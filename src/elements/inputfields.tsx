c805474a604a8d431af8218cc7f8d3c2a4ba33ff
Markiyan <markiyan@pofig.com>
2024-01-21 17:05:15 +0200

    added forgot-password-page

--- src/elements/inputfields.tsx
+++ src/elements/inputfields.tsx
@@ -9,6 +9,8 @@ interface InputProps {
   error: boolean;
   disabled?: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  positionLeft?: string;
+  positionTop?: string;
 }
 
 const Input: FC<InputProps> = ({
@@ -20,10 +22,17 @@ const Input: FC<InputProps> = ({
   error,
   disabled,
   onChange,
+  positionLeft,
+  positionTop,
 }) => {
+  const wrapperStyle = {
+    left: positionLeft,
+    top: positionTop,
+  };
+
   return (
-    <div className="input-wrapper">
-      <label htmlFor={name} className="text-base font-bold font-['Raleway']">
+    <div className="input-wrapper" style={wrapperStyle}>
+      <label htmlFor={name} className="text-base font-bold font-raleway">
         {label}
       </label>
       <input
@@ -34,10 +43,10 @@ const Input: FC<InputProps> = ({
         placeholder={placeholder}
         onChange={onChange}
         disabled={disabled}
-        className="w-[350px] h-[40px] bg-white rounded-[5px] border-2 border-red-400 pl-[11px] pr-[11px] text-base font-bold font-['Raleway']"
+        className="h-10 w-[350px] pl-[11px] pr-[11px] py-[11px] bg-white rounded-[5px] border-2 border-red-400 justify-start items-center inline-flex text-base font-bold font-raleway"
       />
       {error && (
-        <p className="error text-base font-bold font-['Raleway']">
+        <p className="error text-base font-bold font-raleway">
           Input field can't be empty!
         </p>
       )}
