2a18d61a54d8616db63318e45f8677daa7608c6b
Rez1stor <bosco@Boscos-MacBook-Pro.local>
2024-01-22 08:46:37 +0200

    last version

---
+++ src/components/banner.tsx
@@ -0,0 +1,18 @@
+export default function Banner() {
+  return (
+    <>
+      <div
+        id="fon"
+        className=" font-bold tracking-widest text-center content-center grid "
+      >
+        WE SUPPORT UKRAINE
+      </div>
+      <div
+        id="block"
+        className=" font-bold tracking-widest absolute left-0 top-0 w-full text-center content-center grid "
+      >
+        WE SUPPORT UKRAINE
+      </div>
+    </>
+  );
+}
